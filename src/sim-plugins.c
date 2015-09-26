92d20b6c05551951a5dc9bf5fd934005772567f5
Rohan Shah <rohan.shah@hpe.com>
2015-09-26 16:35:09 -0700

    OPS container plugin rework and CT script changes
    Change-Id: I786ab378c63a9562013fb03fa600a55951aff2fa
    Signed-off-by: Rohan Shah <rohan.shah@hpe.com>

--- src/sim-plugins.c
+++ src/sim-plugins.c
@@ -1,18 +1,17 @@
 /*
- *    Copyright (C) 2015 Hewlett-Packard Development Company, L.P.
- *    All Rights Reserved.
+ *  (c) Copyright 2015 Hewlett Packard Enterprise Development LP
  *
- *    Licensed under the Apache License, Version 2.0 (the "License"); you may
- *    not use this file except in compliance with the License. You may obtain
- *    a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *  not use this file except in compliance with the License. You may obtain
+ *  a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *    License for the specific language governing permissions and limitations
- *    under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
  */
 
 #include "openvswitch/vlog.h"
@@ -28,35 +27,36 @@
 #define netdev_register libovs_sim_plugin_LTX_netdev_register
 #define ofproto_register libovs_sim_plugin_LTX_ofproto_register
 
-
 VLOG_DEFINE_THIS_MODULE(sim_plugin);
 
 void
-init(void){
-    /* reset the "ASIC" */
-    system("rm /run/openvswitch-sim/ovsdb.db && \
-            kill -9 `cat /run/openvswitch-sim/ovsdb-server.pid` && \
-            ip netns exec swns iptables -F && sleep 3");
+init(void)
+{
 }
 
 void
-run(void){
+run(void)
+{
 }
 
 void
-wait(void){
+wait(void)
+{
 }
 
 void
-destroy(void){
+destroy(void)
+{
 }
 
 void
-netdev_register(void){
+netdev_register(void)
+{
     netdev_sim_register();
 }
 
 void
-ofproto_register(void){
+ofproto_register(void)
+{
     ofproto_class_register(&ofproto_sim_provider_class);
 }
--- tests/test_switchd_container_ct_lag_access.py
+++ tests/test_switchd_container_ct_lag_access.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-
-# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
-# All Rights Reserved.
+#
+# (c) Copyright 2015 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -17,7 +16,6 @@
 
 from os.path import basename
 import time
-import pytest
 from opsvsi.docker import *
 from opsvsi.opsvsitest import *
 from opsvsiutils.systemutil import *
@@ -58,14 +56,14 @@ class lagAccessTest( OpsVsiTest ):
 
     def config_check(self):
         '''
-            1.1 Add VLAN 100 to global VLAN table on S1 and S2
-            1.2 Add port 1 with tag 100 on S1 and S2
+            1.1 Add VLAN 100 to global VLAN table on Switch1 and Switch2
+            1.2 Add port 1 with tag 100 on Switch1 and Switch2
                 (access mode)
-            1.3 Add port lag0(2,3) with tag 100 on S1 and S2
+            1.3 Add port lag0(2,3) with tag 100 on Switch1 and Switch2
                 (access mode)
             1.4 Check the bridge, VLAN, port and interface tables
                 for both the OVS' and verify correct setting up of
-                the configuration of the SIM OVS by OpenSwitch OVS
+                the configuration of the Internal ASIC OVS by OpenSwitch OVS
         '''
         s1 = self.net.switches[ 0 ]
         s2 = self.net.switches[ 1 ]
@@ -73,7 +71,7 @@ class lagAccessTest( OpsVsiTest ):
         h2 = self.net.hosts[ 1 ]
 
         info("\n########## Test Case 1 - Verify Correct Driving of",
-             "SIM OVS by OpenSwitch OVS ##########\n")
+             "Internal ASIC OVS by OpenSwitch OVS ##########\n")
         info("### Adding ports 1,lag0(2,3) to VLAN100. Port 1:access,",
              "Port lag0(2,3):access ###\n")
         s1.ovscmd("/usr/bin/ovs-vsctl add-br br0")
@@ -113,7 +111,7 @@ class lagAccessTest( OpsVsiTest ):
         assert s1_getvlanadmin_ops == 'up' and s1_getvlanid_ops == '100' \
                and s1_getvlanname_ops == 'VLAN100', \
                "Switch1: OpenSwitch OVS VLAN configuration mismatch"
-        info("### Switch1: OpenSwitch OVS bridge",
+        info("### Switch1: OpenSwitch OVS VLAN",
              "configuration correctly set ###\n")
 
         s1_getportname_ops, s1_getporttag_ops, s1_getportvlanmode_ops = \
@@ -145,275 +143,41 @@ class lagAccessTest( OpsVsiTest ):
         info("### Switch1: Dual OVS interface configuration",
              "correctly set ###\n")
 
-        # OPS_TODO: LAG tests require extra test cases where we set the bond
-        # configuration on the fly. For eg: Bringing one of the
-        # interfaces link down from the bond and verify the bond configuration.
-        # Currently, the bond configuration does not support
-        # change of port/bond configuration dynamically. Once, the VSI provider
-        # codes have this functionality, more test cases will be added
-
-        s1.ovscmd("/usr/bin/ovs-vsctl del-br br0")
-        s2.ovscmd("/usr/bin/ovs-vsctl del-br br0")
-
-    def add_ports_and_vlan(self):
-        '''
-            2.1 Add VLAN 100 to global VLAN table on S1 and S2
-            2.2 Add port 1 with tag 100 on S1 and S2 (access mode)
-            2.3 Add port lag0(2,3) with tag 100 on S1 and S2 (access mode)
-            2.4 Test Ping - should work
-        '''
-        s1 = self.net.switches[ 0 ]
-        s2 = self.net.switches[ 1 ]
-        h1 = self.net.hosts[ 0 ]
-        h2 = self.net.hosts[ 1 ]
-
-        info("\n######### Test Case 2 - Adding VLAN and Ports #########\n")
-        info("### Adding ports 1,lag0(2,3) to VLAN100. Port 1:access,",
-             "Port lag0(2,3):access ###\n")
-        info("### Adding VLAN100 to the VLAN table and adding ports1,",
-             "lag(2,3) to bridge: br0\n")
-        info("### In access mode and running ping traffic between",
-             "the hosts: h1, h2 ###\n")
-
-        '''s1.ovscmd("/usr/bin/ovs-vsctl add-br br0")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-br br0")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-port br0 1 vlan_mode=access " \
-            "tag=100 -- set interface 1 user_config:admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-port br0 1 vlan_mode=access " \
-            "tag=100 -- set interface 1 user_config:admin=up")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")'''
-
-        out = h1.cmd("ping -c1 %s" % h2.IP())
-        info(out)
-        status = parsePing(out)
-        assert status, "Failed to transmit packets when " \
-                       "LAG and port are in access mode"
-        info("### Ping Success ###\n")
-
-
-        s1.ovscmd("/usr/bin/ovs-vsctl del-br br0")
-        s2.ovscmd("/usr/bin/ovs-vsctl del-br br0")
-
-    def del_and_readd_vlan(self):
-        '''
-            3.1 Dont add VLAN 100 to global VLAN table of S1 and S2
-            3.2 Add port 1 with tag 100 on S1 and S2 (access mode)
-            3.3 Add port lag0(2,3) with tag 100 on S1 and S2 (access mode)
-            3.4 Test Ping - should not work
-            3.5 Add VLAN 100 to global VLAN table of S1 and S2
-            3.6 Test Ping - should work
-        '''
-        s1 = self.net.switches[ 0 ]
-        s2 = self.net.switches[ 1 ]
-        h1 = self.net.hosts[ 0 ]
-        h2 = self.net.hosts[ 1 ]
-
-        info("\n######### Test Case 3 - Without Global VLAN #########\n")
-        info("### Deleting VLAN100 from the VLAN table.",
-             "Ports get deleted from the SIM OVS ###\n")
-        info("### Ping traffic is expected to fail as the ports",
-             "get deleted from the SIM OVS ###\n\n")
-
-        '''s1.ovscmd("/usr/bin/ovs-vsctl add-br br0")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-br br0")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-port br0 1 vlan_mode=access " \
-            "tag=100 -- set interface 1 user_config:admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-port br0 1 vlan_mode=access " \
-            "tag=100 -- set interface 1 user_config:admin=up")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")'''
-
+        info("\n\n### Deleting VLAN100. Bond is expected to",
+             "get deleted from Internal ASIC OVS ###")
         s1.ovscmd("/usr/bin/ovs-vsctl del-vlan br0 100")
-        time.sleep(2)
+        out = s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl get " \
+        "interface 2 admin_state").strip()
+        assert "no row 2 in table Interface" in out, "Dual OVS port configuration \
+                not deleted even after deleting VLAN"
+        info("\n### Switch1: ASIC OVS bond configuration correctly deleted ###\n")
+
+        info("\n\n### Re-adding VLAN100. Bond is expected to",
+             "get reconfigured and added in Internal ASIC OVS ###")
+        s1.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
+        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl " \
+            "get port lag0 name tag vlan_mode").splitlines()
+
+        assert s1_getportname_ops == s1_getportname_openovs \
+               and s1_getporttag_ops == s1_getporttag_openovs \
+               and s1_getportvlanmode_ops == s1_getportvlanmode_openovs, \
+               "Switch1: ASIC OVS bond configuration mismatch after re-adding VLAN"
+        info("\n### Switch1: ASIC OVS bond configuration correctly re-set ###\n\n\n\n")
+
+        #Cleanup before next test
+        s1.ovscmd("/usr/bin/ovs-vsctl del-vlan br0 100")
         s2.ovscmd("/usr/bin/ovs-vsctl del-vlan br0 100")
-        time.sleep(2)
-
-        out = h1.cmd("ping -c1 %s" % h2.IP())
-        info(out)
-        status = parsePing(out)
-        assert not status, "After deleting VLAN, SIM OVS ports " \
-                       "are expected to get reconfigured/deleted." \
-                       "Successful ping implies test failure"
-        info("### Failure in transmitting packets implies",
-             "successful test execution ###\n")
-
-
-        info("### Adding Global VLAN ###\n")
-        info("### Adding VLAN100. Ports 1,lag0(2,3) get reconfigured",
-             "with tag=100 ###\n")
-        info("### Ping traffic is expected to pass as the ports",
-             "get re-added in the SIM OVS ###\n\n")
-
-        s1.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
-        time.sleep(2)
-        s2.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
-        time.sleep(2)
-
-        # OPS_TODO: Remove the explicit driving of SIM OVS
-        # once the LAG state info is taken care of
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 2")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 2")
-        time.sleep(2)
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 lag0")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 lag0")
-        time.sleep(2)
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100")
-        time.sleep(2)
-
-        out = h1.cmd("ping -c1 %s" % h2.IP())
-        info(out)
-        status = parsePing(out)
-        assert status, "Failed to transmit packets even after readdition " \
-                       "of VLAN. Port and LAG are expected to get reconfigured"
-        info("### Ping Success ###\n")
-
+        s1.ovscmd("/usr/bin/ovs-vsctl del-port 1")
+        s2.ovscmd("/usr/bin/ovs-vsctl del-port 1")
+        s1.ovscmd("/usr/bin/ovs-vsctl del-port 2")
+        s2.ovscmd("/usr/bin/ovs-vsctl del-port 2")
         s1.ovscmd("/usr/bin/ovs-vsctl del-br br0")
         s2.ovscmd("/usr/bin/ovs-vsctl del-br br0")
 
 
-    def diff_and_same_tags(self):
-        '''
-            4.1 Add VLAN 100 to global VLAN table in S1 and S2
-            4.2 Add port 1 with tag 100 on S1 and S2 (access mode)
-            4.3 Add port lag0(2,3) with tag 200 on S1 and S2 (access mode)
-            4.4 Test Ping - should not work
-            4.5 Delete port lag0(2,3) and add port lag0(2,3)
-                with tag 100 on S1 and S2 (access mode)
-            4.6 Test Ping - should work
-        '''
-        s1 = self.net.switches[ 0 ]
-        s2 = self.net.switches[ 1 ]
-        h1 = self.net.hosts[ 0 ]
-        h2 = self.net.hosts[ 1 ]
+        # Note: Once we support Static LAGs in ASIC OVS, more
+        # test cases can be added.
 
-        info("\n########## Test Case 4 - With Different Tags ##########\n")
-        info("### Adding S1->Port 1:tag=100.",
-             "S2->Port lag0(2,3):tag=200 ###\n")
-        info("### Ping traffic is expected to fail as the",
-             "ports get added with different tags ###\n\n")
-
-        '''s1.ovscmd("/usr/bin/ovs-vsctl add-br br0")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-br br0")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-vlan br0 100 admin=up")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-port br0 1 vlan_mode=access " \
-            "tag=100 -- set interface 1 user_config:admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-port br0 1 vlan_mode=access " \
-            "tag=100 -- set interface 1 user_config:admin=up")
-        s1.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")
-        s2.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")'''
-
-        s1.ovscmd("/usr/bin/ovs-vsctl del-port lag0")
-        time.sleep(2)
-        s2.ovscmd("/usr/bin/ovs-vsctl del-port lag0")
-        time.sleep(2)
-        s1.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")
-        time.sleep(2)
-        s2.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=200 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")
-        time.sleep(2)
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 2")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 2")
-        time.sleep(2)
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 lag0")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 lag0")
-        time.sleep(2)
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl add-bond br0 lag0 " \
-               "2 3 vlan_mode=access tag=100")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl add-bond br0 lag0 " \
-               "2 3 vlan_mode=access tag=200")
-        time.sleep(2)
-        out = h1.cmd("ping -c1 %s" % h2.IP())
-        info(out)
-        status = parsePing(out)
-        assert not status, "LAG ports on both the switch have " \
-               "different tags. Successful ping implies test failure"
-        info("### Failure in transmitting packets implies",
-             "successful test execution ###\n")
-
-
-        info("### Changing Tags Back to Original ###\n")
-        info("### Adding Port lag0 in S1, S2 with tag=100.",
-             "Ports get reconfigured ###\n")
-        info("### Ping traffic is expected to pass as the ports",
-             "get re-added to the SIM OVS with same tags ###\n")
-
-        s1.ovscmd("/usr/bin/ovs-vsctl del-port lag0")
-        time.sleep(2)
-        s2.ovscmd("/usr/bin/ovs-vsctl del-port lag0")
-        time.sleep(2)
-        s1.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")
-        time.sleep(2)
-        s2.ovscmd("/usr/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100 " \
-               "-- set interface 2 user_config:admin=up " \
-               "-- set interface 3 user_config:admin=up")
-        time.sleep(2)
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 2")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 2")
-        time.sleep(2)
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 lag0")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl del-port br0 lag0")
-        time.sleep(2)
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100")
-        time.sleep(2)
-        s2.ovscmd("/opt/openvswitch/bin/ovs-vsctl add-bond br0 lag0 2 3 " \
-               "vlan_mode=access tag=100")
-        out = h1.cmd("ping -c1 %s" % h2.IP())
-        info(out)
-        status = parsePing(out)
-        assert status, "LAG ports on both the switches have same tags. " \
-               "Packets should get transmitted. Test execution failure"
-        info("### Ping Success ###\n")
-
-        s1.ovscmd("/usr/bin/ovs-vsctl del-br br0")
-        s2.ovscmd("/usr/bin/ovs-vsctl del-br br0")
-
-@pytest.mark.skipif(True, \
-reason="Skipping %s as it does not work" % basename(__file__))
 class Test_switchd_container_lag_access:
 
     def setup_class(cls):
@@ -421,15 +185,6 @@ class Test_switchd_container_lag_access:
 
     def test_switchd_container_config_check(self):
         self.test.config_check()
-
-    def test_switchd_container_normal(self):
-        self.test.add_ports_and_vlan()
-
-    def test_switchd_container_missing_vlan(self):
-        self.test.del_and_readd_vlan()
-
-    def test_switchd_container_tags(self):
-        self.test.diff_and_same_tags()
 
     def teardown_class(cls):
         # Stop the Docker containers, and
