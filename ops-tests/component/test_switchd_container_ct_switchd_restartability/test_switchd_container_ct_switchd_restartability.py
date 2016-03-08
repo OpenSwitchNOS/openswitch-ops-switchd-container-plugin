925e85fc49ca4bff837054280ea791f4cd72d499
fonsecamau <fonsecamau@hpe.com>
2016-03-07 16:50:15 -0800

    Converted tests
    Change-Id: I0ac23dbbbcd2aebb4749034885dcfb90a9770fd1
    Signed-off-by: fonsecamau <fonsecamau@hpe.com>

---
+++ ops-tests/component/test_switchd_container_ct_switchd_restartability/test_switchd_container_ct_switchd_restartability.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+# (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+##########################################################################
+
+"""
+OpenSwitch Test for switchd related configurations.
+"""
+
+# from pytest import mark
+from time import sleep
+
+TOPOLOGY = """
+# +-------+
+# |  ops1 |
+# +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+"""
+
+
+def test_switchd_container_ct_switchd_restartability(topology, step):
+    ops1 = topology.get("ops1")
+    assert ops1 is not None
+
+    step("Step 1- Adding VLAN100 and ports 1, 2, 3 to default bridge")
+    ops1("add-vlan bridge_normal 100 admin=up", shell="vsctl")
+    ops1("add-port bridge_normal 1", shell="vsctl")
+    ops1("set interface 1 user_config:admin=up", shell="vsctl")
+    ops1("add-port bridge_normal 2", shell="vsctl")
+    ops1("set interface 2 user_config:admin=up", shell="vsctl")
+    ops1("add-port bridge_normal 3", shell="vsctl")
+    ops1("set interface 3 user_config:admin=up", shell="vsctl")
+
+    step("Step 2- Stopping switchd service")
+    ops1("systemctl stop switchd", shell="bash")
+
+    step("Step 3- Deleting port 3 from the OpenSwitch database")
+    ops1("ovs-vsctl --no-wait del-port bridge_normal 3", shell="bash")
+
+    step("Step 4- Verifying that port 3 still exists in the 'ASIC' OVS "
+         "database")
+    port_name = ops1("/opt/openvswitch/bin/ovs-vsctl get port 3 vlan_mode",
+                     shell="bash")
+    assert "trunk" in port_name
+
+    step("Step 5- 'Starting' switchd service")
+    ops1("systemctl start switchd", shell="bash")
+
+    step("Step 6- Verifying that port 3 got deleted from the 'ASIC' OVS "
+         "database")
+    port_name = ops1("/opt/openvswitch/bin/ovs-vsctl get port 3 vlan_mode",
+                     shell="bash")
+    assert "trunk" not in port_name
+
+    step("Step 7- Stopping switchd service")
+    ops1("systemctl stop switchd", shell="bash")
+
+    step("Step 8- Deleting port 2 from the OpenSwitch database")
+    ops1("--no-wait del-port bridge_normal 2", shell="vsctl")
+
+    step("Step 9- Verifying that port 2 still exists in the 'ASIC' OVS "
+         "database")
+    port_name = ops1("/opt/openvswitch/bin/ovs-vsctl get port 2 vlan_mode",
+                     shell="bash")
+    assert "trunk" in port_name
+
+    step("Step 10- 'Re-starting' switchd service")
+    ops1("systemctl restart switchd", shell="bash")
+
+    step("Step 11- Verifying that port 2 got deleted from the 'ASIC' OVS "
+         "database")
+    port_name = ops1("/opt/openvswitch/bin/ovs-vsctl get port 2 vlan_mode",
+                     shell="bash")
+    assert "trunk" not in port_name
+
+    step("Step 1- Taking note of 'pid' of ops-switchd, ovs-vswitchd-sim and "
+         "'ASIC' ovsdb-server processes before killing OpenSwitch "
+         "switchd service")
+    pid_switchd_bf = ops1("pidof ops-switchd", shell="bash")
+    pid_vswitchd_sim_bf = ops1("pidof ovs-vswitchd-sim", shell="bash")
+    pid_ovsdb_sim_bf = ops1("pidof /opt/openvswitch/sbin/ovsdb-server",
+                            shell="bash")
+
+    step("Step 2- Stopping 'ASIC' ovsdb-server service")
+    ops1("systemctl stop ovsdb-server-sim", shell="bash")
+
+    step("Step 3- Deleting 'ASIC' OVS database file")
+    ops1("sudo rm -rf /var/run/openvswitch-sim/ovsdb.db", shell="bash")
+
+    step("Step 4- Killing switchd service which is expected to restart by "
+         "itself")
+    ops1("sudo kill -9 $(ps -A | grep ops-switchd | awk '{print $1}')",
+         shell="bash")
+    sleep(2)
+
+    step("Step 5- Taking note of 'pid' of ops-switchd, ovs-vswitchd-sim and "
+         "ovsdb-server-sim services after OpenSwitch switchd "
+         "service restarted")
+    pid_switchd_af = ops1("pidof ops-switchd", shell="bash")
+    pid_vswitchd_sim_af = ops1("pidof ovs-vswitchd-sim", shell="bash")
+    pid_ovsdb_sim_af = ops1("pidof /opt/openvswitch/sbin/ovsdb-server",
+                            shell="bash")
+
+    step("Step 6- Verifying if new processes were created when switchd "
+         "restarts by itself")
+    assert (
+        str(pid_switchd_bf) != str(pid_switchd_af) and
+        str(pid_vswitchd_sim_bf) != str(pid_vswitchd_sim_af) and
+        str(pid_ovsdb_sim_bf) != str(pid_ovsdb_sim_af)
+    )
+
+    step("Step 7- Verifying if the 'ASIC' OVS data is in sync with the "
+         "OpenSwitch OVS")
+    port_name = ops1("/opt/openvswitch/bin/ovs-vsctl get port 1 vlan_mode",
+                     shell="bash")
+    assert "trunk" in port_name
---
+++ ops-tests/component/test_switchd_container_ct_vlan_access/test_switchd_ct_vlan_access_check_config.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+##########################################################################
+
+"""
+OpenSwitch Test for switchd related configurations.
+"""
+
+# from pytest import set_trace
+from time import sleep
+
+TOPOLOGY = """
+# +-------+
+# |  ops1 |
+# +-------+
+
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+"""
+
+
+def test_switchd_ct_vlan_access_check_config(topology, step):
+    ops1 = topology.get("ops1")
+    assert ops1 is not None
+
+    step("Add br0 on switch")
+    ops1("add-br br0", shell="vsctl")
+
+    ops_br_name = ops1("get br br0 name", shell="vsctl").strip()
+    out = ops1("/opt/openvswitch/bin/ovs-vsctl get br br0 datapath_type name",
+               shell="bash")
+    ovs_datapath_type, ovs_br_name = out.splitlines()
+    assert (
+        ovs_datapath_type == 'netdev' and
+        ops_br_name == ovs_br_name
+    )
+
+    step("Add VLAN100 to global VLAN table on Switch")
+    ops1("add-vlan br0 100 admin=up", shell="vsctl")
+    out = ops1("get vlan VLAN100 admin id name", shell="vsctl")
+    ops_admin_state, ops_vlan_id, ops_vlan_name = out.splitlines()
+    assert (
+        ops_admin_state == 'up' and
+        ops_vlan_id == '100' and
+        ops_vlan_name == '"VLAN100"'
+    )
+
+    step("Add port 1 and 2 with tag 100 on Switch (access mode)")
+    ops1("add-port br0 1 vlan_mode=access tag=100", shell="vsctl")
+    ops1("set interface 1 user_config:admin=up", shell="vsctl")
+    sleep(1)
+
+    out = ops1("get port 1 name tag vlan_mode", shell="vsctl")
+    ops_port_name, ops_tag, ops_vlan_mode = out.splitlines()
+    ops1("/opt/openvswitch/bin/ovs-vsctl get port 1 name tag vlan_mode",
+         shell="bash")
+    ovs_port_name, ovs_tag, ovs_vlan_mode = out.splitlines()
+    assert (
+        ops_port_name == ovs_port_name and
+        ops_tag == ovs_tag and
+        ops_vlan_mode == ovs_vlan_mode
+    )
+
+    step("Verify whether same configuration gets set on the ASIC"
+         " simulating InternalOVS")
+    out = ops1("get interface 1 admin_state link_state user_config:admin "
+               "hw_intf_info:mac_addr", shell="vsctl")
+    x, ops_admin_state, ops_link_state, ops_user_config, ops_mac_addr = (
+        out.splitlines()
+    )
+    print(x)
+    out = ops1("/opt/openvswitch/bin/ovs-vsctl get interface 1 "
+               "admin_state mac_in_use", shell="bash")
+    x, ovs_admin_state, ovs_mac_addr = out.splitlines()
+    print(x)
+    assert (
+        ops_admin_state == ovs_admin_state and
+        ops_link_state == 'up' and
+        ops_user_config == 'up' and
+        ops_mac_addr == ovs_mac_addr
+    )
