25c3c6b09e550639e40399414fd7484c6001a49f
Rohan Shah <rohan.shah@hpe.com>
2016-03-08 17:51:07 -0800

    new: usr: Taiga Bug# 389. Adding tests to verify the admin and link states for internal VLAN interfaces.
    src/ops-switchd-container-plugin/tests/test_switchd_container_ct_changing_vlan_config.py:184: AssertionError
    ================================================================================================= 3 failed, 22 passed in 635.33 seconds
    
    Change-Id: I72f3ed66a70f6b11d5e4ad5582c113c5f1936225
    Signed-off-by: Rohan Shah <rohan.shah@hpe.com>

--- tests/test_switchd_container_ct_intervlan.py
+++ tests/test_switchd_container_ct_intervlan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# (c) Copyright 2015 Hewlett Packard Enterprise Development LP
+# (c) Copyright 2015-2016 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -19,159 +19,208 @@ from opsvsi.docker import *
 from opsvsi.opsvsitest import *
 from opsvsiutils.systemutil import *
 
-class simIntervlanTests( OpsVsiTest ):
 
-  def setupNet(self):
-    host_opts = self.getHostOpts()
-    switch_opts = self.getSwitchOpts()
-    vlan_topo = SingleSwitchTopo(k=2, hopts=host_opts, sopts=switch_opts)
-    self.net = Mininet(vlan_topo, switch=VsiOpenSwitch,
-                       host=Host, link=OpsVsiLink,
-                       controller=None, build=True)
-
-  def intervlan_direct_connected(self):
-    info("########## Verify intervlan for directly",
-         "connected hosts ##########\n")
-    # configuring OpenSwitch, in the future it would be through
-    # proper OpenSwitch commands
-    s1 = self.net.switches[ 0 ]
-    h1 = self.net.hosts[ 0 ]
-    h2 = self.net.hosts[ 1 ]
-    # Configure switch s1
-    s1.cmdCLI("configure terminal")
-
-    # Configure VLANS 100 and 200 on siwtch
-    s1.cmdCLI("vlan 100")
-    s1.cmdCLI("no shutdown")
-    s1.cmdCLI("exit")
-
-    s1.cmdCLI("vlan 200")
-    s1.cmdCLI("no shutdown")
-    s1.cmdCLI("exit")
+def checkPing(pingOutput):
+    '''Parse ping output and check to see if one of the pings succeeded or failed'''
+    # Check for downed link
+    if 'Destination Host Unreachable' in pingOutput:
+        return False
+    r = r'(\d+) packets transmitted, (\d+) received'
+    m = re.search(r, pingOutput)
+    if m is None:
+        return False
+    sent, received = int(m.group(1)), int(m.group(2))
+    if sent >= 1 and received >= 1:
+        return True
+    else:
+        return False
 
 
-    # Configure interface 1 on switch s1
-    s1.cmdCLI("interface 1")
-    s1.cmdCLI("no routing")
-    s1.cmdCLI("no shutdown")
-    s1.cmdCLI("vlan access 100")
-    s1.cmdCLI("exit")
+class simIntervlanTests(OpsVsiTest):
 
-    # Configure interface 2 on switch s1
-    s1.cmdCLI("interface 2")
-    s1.cmdCLI("no routing")
-    s1.cmdCLI("no shutdown")
-    s1.cmdCLI("vlan access 200")
-    s1.cmdCLI("exit")
+    def setupNet(self):
+        host_opts = self.getHostOpts()
+        switch_opts = self.getSwitchOpts()
+        vlan_topo = SingleSwitchTopo(k=2, hopts=host_opts, sopts=switch_opts)
+        self.net = Mininet(vlan_topo, switch=VsiOpenSwitch,
+                           host=Host, link=OpsVsiLink,
+                           controller=None, build=True)
 
-    info("Configuring interface vlan 100\n")
-    time.sleep(5)
-    # Configure interface vlan 100 and vlan 200 on switch s1
-    s1.cmdCLI("interface vlan 100")
-    s1.cmdCLI("ip address 100.0.0.1/24")
-    time.sleep(5)
-    s1.cmdCLI("ipv6 address 100::1/120")
-    s1.cmdCLI("exit")
+    def intervlan_direct_connected(self):
+        info("########## Verify intervlan for directly",
+             "connected hosts ##########\n")
+        s1 = self.net.switches[0]
+        h1 = self.net.hosts[0]
+        h2 = self.net.hosts[1]
+        # Configure switch s1
+        s1.cmdCLI("configure terminal")
 
-    time.sleep(5)
+        # Configure VLANs 100 and 200 on switch
+        s1.cmdCLI("vlan 100")
+        s1.cmdCLI("no shutdown")
+        s1.cmdCLI("exit")
 
-    info("Configuring interface vlan 200\n")
-    s1.cmdCLI("interface vlan 200")
-    s1.cmdCLI("ip address 200.0.0.1/24")
-    time.sleep(5)
-    s1.cmdCLI("ipv6 address 200::1/120")
-    s1.cmdCLI("exit")
+        s1.cmdCLI("vlan 200")
+        s1.cmdCLI("no shutdown")
+        s1.cmdCLI("exit")
 
-    # Check trunks list on port bridge normal in native OVS
-    output = \
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl list port bridge_normal")
-    assert "100, 200" in output, \
-           "Trunk vlan 100 on bridge_normal wasnt configured"
+        # Configure interface 1 on switch s1
+        s1.cmdCLI("interface 1")
+        s1.cmdCLI("no routing")
+        s1.cmdCLI("no shutdown")
+        s1.cmdCLI("vlan access 100")
+        s1.cmdCLI("exit")
 
-    info("\n### vlan interface configuration: Success ###\n")
+        # Configure interface 2 on switch s1
+        s1.cmdCLI("interface 2")
+        s1.cmdCLI("no routing")
+        s1.cmdCLI("no shutdown")
+        s1.cmdCLI("vlan access 200")
+        s1.cmdCLI("exit")
 
-    # Bring interface 1 up
-    s1.ovscmd("/usr/bin/ovs-vsctl set interface 1 user_config:admin=up")
+        info("Configuring interface vlan 100\n")
+        time.sleep(5)
+        # Configure interface vlan 100 and vlan 200 on switch s1
+        s1.cmdCLI("interface vlan 100")
+        s1.cmdCLI("ip address 100.0.0.1/24")
+        time.sleep(5)
+        s1.cmdCLI("ipv6 address 100::1/120")
+        s1.cmdCLI("exit")
 
-    # Bring interface 2 up
-    s1.ovscmd("/usr/bin/ovs-vsctl set interface 2 user_config:admin=up")
+        time.sleep(5)
 
-    # Configure host 1
-    info("Configuring host 1 with 100.0.0.2/24\n")
-    h1.cmd("ip addr add 100.0.0.2/24 dev h1-eth0")
-    h1.cmd("ip route add 200.0.0.0/24 via 100.0.0.1");
-    info("Configuring host 1 with 100::2/120\n")
-    h1.cmd("ip addr add 100::2/120 dev h1-eth0")
-    h1.cmd("ip route add 200::0/120 via 100::1")
+        info("Configuring interface vlan 200\n")
+        s1.cmdCLI("interface vlan 200")
+        s1.cmdCLI("ip address 200.0.0.1/24")
+        time.sleep(5)
+        s1.cmdCLI("ipv6 address 200::1/120")
+        s1.cmdCLI("exit")
+        time.sleep(5)
 
-    # Configure host 2
-    info("Configuring host 2 with 200.0.0.2/24\n")
-    h2.cmd("ip addr add 200.0.0.2/24 dev h2-eth0")
-    h2.cmd("ip route add 100.0.0.0/24 via 200.0.0.1");
-    info("Configuring host 2 with 200::2/120\n")
-    h2.cmd("ip addr add 200::2/120 dev h2-eth0")
-    h2.cmd("ip route add 100::0/120 via 200::1")
+        # Check trunks list on port bridge normal in native OVS
+        output = \
+            s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl list port bridge_normal")
+        assert "100, 200" in output, \
+               "Trunk vlan 100 on bridge_normal wasnt configured"
 
-    # Ping from host 1 to host2
-    info("Ping h2 from h1\n")
-    output = h1.cmd("ping 200.0.0.2 -c2")
-    status = parsePing(output)
-    assert status, "Ping Failed\n"
-    info("Ping Success\n")
+        info("\n### vlan interface configuration: Success ###\n")
 
-    # Ping6 from host 1 to host 2
-    info("IPv6 Ping h2 from h1\n")
-    output = h1.cmd("ping6 200::2 -c2")
-    status = parsePing(output)
-    #assert status, "Pingi6 Failed"
-    info("Ping6 Success\n")
+        # Configure host 1
+        info("Configuring host 1 with 100.0.0.2/24\n")
+        h1.cmd("ip addr add 100.0.0.2/24 dev h1-eth0")
+        h1.cmd("ip route add 200.0.0.0/24 via 100.0.0.1")
+        info("Configuring host 1 with 100::2/120\n")
+        h1.cmd("ip addr add 100::2/120 dev h1-eth0")
+        h1.cmd("ip route add 200::0/120 via 100::1")
 
-    # Unconfigure interface vlan 100
-    info("Unconfigure vlan interface.",
-         "This will disable inter-vlan routing.\n")
-    s1.cmdCLI("no interface vlan 100")
-    s1.cmdCLI("no interface vlan 200")
+        # Configure host 2
+        info("Configuring host 2 with 200.0.0.2/24\n")
+        h2.cmd("ip addr add 200.0.0.2/24 dev h2-eth0")
+        h2.cmd("ip route add 100.0.0.0/24 via 200.0.0.1")
+        info("Configuring host 2 with 200::2/120\n")
+        h2.cmd("ip addr add 200::2/120 dev h2-eth0")
+        h2.cmd("ip route add 100::0/120 via 200::1")
 
-    time.sleep(5)
-    # Check trunks list on port bridge normal in native OVS
-    output = \
-        s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl list port bridge_normal")
-    assert "100, 200" not in output, \
-           "Trunk vlans 100, 200 on bridge_normal still configured"
+        # Ping from host 1 to host2
+        info("Ping h2 from h1\n")
+        output = h1.cmd("ping 200.0.0.2 -c5")
+        status = checkPing(output)
+        assert status, "Ping Failed\n"
+        info("Ping Success\n")
 
+        # Ping6 from host 1 to host 2
+        info("IPv6 Ping h2 from h1\n")
+        output = h1.cmd("ping6 200::2 -c5")
+        status = checkPing(output)
+        # assert status, "Pingi6 Failed"
+        info("Ping6 Success\n")
 
-    # Ping from host 1 to host2
-    info("Check IPv4 ping h2 from h1\n")
-    output = h1.cmd("ping 200.0.0.2 -c2")
-    status = parsePing(output)
-    assert status == 0, \
-           "Ping succeeded even though vlan interface is not present``\n"
-    info("Ipv4 routing between vlans disabled\n")
+        # Unconfigure interface vlan 100
+        info("Unconfigure vlan interface.",
+             "This will disable inter-vlan routing.\n")
+        s1.cmdCLI("no interface vlan 100")
+        s1.cmdCLI("no interface vlan 200")
 
-    # Ping6 from host 1 to host 2
-    info("Check IPv6 Ping h2 from h1\n")
-    output = h1.cmd("ping6 2000::2 -c2")
-    status = parsePing(output)
-    assert status == 0, \
-           "Ping6 succeeded even though vlan interface is not present`\n"
-    info("Ipv6 routing between vlans disabled\n")
-    info("Delete vlan interface Success\n")
+        time.sleep(5)
+        # Check trunks list on port bridge normal in native OVS
+        output = \
+            s1.ovscmd("/opt/openvswitch/bin/ovs-vsctl list port bridge_normal")
+        assert "100, 200" not in output, \
+               "Trunk vlans 100, 200 on bridge_normal still configured"
 
+        # Ping from host 1 to host2
+        info("Check IPv4 ping h2 from h1\n")
+        output = h1.cmd("ping 200.0.0.2 -c5")
+        status = checkPing(output)
+        assert status == 0, \
+            "Ping succeeded even though vlan interface is not present``\n"
+        info("Ipv4 routing between vlans disabled\n")
 
-    info("########## inter-vlan test for directly",
-         "connected hosts passed ##########\n")
+        # Ping6 from host 1 to host 2
+        info("Check IPv6 Ping h2 from h1\n")
+        output = h1.cmd("ping6 2000::2 -c5")
+        status = checkPing(output)
+        assert status == 0, \
+            "Ping6 succeeded even though vlan interface is not present`\n"
+        info("Ipv6 routing between vlans disabled\n")
+        info("Delete vlan interface Success\n")
+
+        s1.cmdCLI("end")
+
+        info("\n########## Inter-vlan test for directly",
+             "connected hosts passed ##########\n\n")
+
+    def intervlan_admin_and_link_states(self):
+        info("\n########## Verify interface VLAN admin and link states ",
+             "##########\n")
+        '''
+    This function verifies if the admin and the link state for the interface
+    VLANs are "up" after "no shut" and "down" after "shutdown". This helps
+    zebra in selecting the interface VLAN as a nexthop for forwarding.
+    '''
+        s1 = self.net.switches[0]
+        h1 = self.net.hosts[0]
+        h2 = self.net.hosts[1]
+
+        # Configuring VLAN interfaces and bringing them up
+        info("### Bringing the interface VLAN up ###\n")
+        s1.cmdCLI("configure terminal")
+        s1.cmdCLI("interface vlan 100")
+        s1.cmdCLI("no shutdown")
+        time.sleep(5)
+        admin_state, link_state = s1.ovscmd("ovs-vsctl get interface vlan100 "
+                                            "admin_state link_state").splitlines()
+        assert "up" in admin_state and "up" in link_state, \
+            "Admin and Link State Verification Unsuccessful\n\n"
+
+        # Configuring VLAN interfaces and bringing them up
+        info("### Bringing the interface VLAN down ###\n")
+        s1.cmdCLI("shutdown")
+        time.sleep(5)
+        admin_state, link_state = s1.ovscmd("ovs-vsctl get interface vlan100 "
+                                            "admin_state link_state").splitlines()
+        assert "down" in admin_state and "down" in link_state, \
+            "Admin and Link State Verification Unsuccessful\n\n"
+
+        info("\n########## Interface VLAN test for admin",
+             "and link states passed ##########\n\n")
+
 
 class Test_switchd_container_intervlan:
 
-  def setup_class(cls):
-    Test_switchd_container_intervlan.test = simIntervlanTests()
+    def setup_class(cls):
+        Test_switchd_container_intervlan.test = simIntervlanTests()
 
-  # Test for slow routing between directly connected hosts
-  def test_switchd_container_intervlan_direct_connected(self):
-    self.test.intervlan_direct_connected()
+    # Test for slow routing between directly connected hosts
+    def test_switchd_container_intervlan_direct_connected(self):
+        self.test.intervlan_direct_connected()
 
-  def teardown_class(cls):
-    Test_switchd_container_intervlan.test.net.stop()
+    # Test to verify the admin and the link states for interface VLANs
+    def test_switchd_container_intervlan_admin_and_link_states(self):
+        self.test.intervlan_admin_and_link_states()
 
-  def __del__(self):
-    del self.test
+    def teardown_class(cls):
+        Test_switchd_container_intervlan.test.net.stop()
+
+    def __del__(self):
+        del self.test
