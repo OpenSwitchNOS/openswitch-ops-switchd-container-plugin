6d6a08e908730534427bf83c25fb165e3701126d
Ricky Charlet <ricky.charlet@hpe.com>
2016-03-28 18:04:34 -0700

    vxlan container POC. Initial checkin
    This implements the container plugin of the vxlan POC. This
    is a port of MyHanh's code. She originally made her code
    work before the openvswitch 2.5 uprev. So this port differs
    slightly.
    
    Change-Id: If1281ee294a9249ee0ae459444b2fe919167d542
    Signed-off-by: Ricky Charlet <ricky.charlet@hpe.com>

---
+++ include/netdev-vport-sim.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2010, 2011, 2012, 2013 Nicira, Inc.
+ * Copyright (C) 2016 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NETDEV_VPORT_SIM_H
+#define NETDEV_VPORT_SIM_H 1
+
+
+/* SIM provider API. */
+void netdev_vport_sim_register(void);
+void netdev_vport_patch_register(void);
+
+#endif /* netdev-sim.h */
